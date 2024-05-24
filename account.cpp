07a9e330d47cb5062782af456c275e75d0c02ca0
Hartmann <hartmann_psi@sjtu.edu.cn>
2024-05-24 16:52:41 +0800

    调试缓存参数

--- account.cpp
+++ account.cpp
@@ -1,7 +1,7 @@
 #pragma GCC optimize(3)
 #include "account.hpp"
 
-AcSys::AcSys() : data("account.dat", "account.r", 1145) {}
+AcSys::AcSys() : data("account.dat", "account.r", 800) {}
 
 AcSys::~AcSys() = default;
 
