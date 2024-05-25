b1b9cdaac2f9133a16835fc0f2fb00de2a50ab3e
Hartmann <hartmann_psi@sjtu.edu.cn>
2024-05-25 13:41:55 +0800

    测试使用std::vector的性能

--- vector.hpp
+++ vector.hpp
@@ -4,7 +4,10 @@
 #include <cstddef>
 #include <cstring>
 #include <string>
+#include <vector>
 
+using std::vector;
+/*
 namespace sjtu {
 
 class exception {
@@ -19,36 +22,26 @@ public:
 };
 
 class index_out_of_bound : public exception {
-  /* __________________________ */
+
 };
 
 class runtime_error : public exception {
-  /* __________________________ */
+
 };
 
 class invalid_iterator : public exception {
-  /* __________________________ */
+
 };
 
 class container_is_empty : public exception {
-  /* __________________________ */
+
 };
 } // namespace sjtu
 
-/**
- * a data container like std::vector
- * store data in a successive memory and support random access.
- */
+
 template <typename T> class vector {
 public:
-  /**
-   * TODO
-   * a type for actions of the elements of a vector, and you should write
-   *   a class named const_iterator with same interfaces.
-   */
-  /**
-   * you can see RandomAccessIterator at CppReference for help.
-   */
+
   class const_iterator;
   class iterator {
     // The following code is written for the C++ type_traits library.
@@ -79,10 +72,7 @@ public:
   public:
     iterator(const std::size_t &ptr_, vector<T> *father_)
         : ptr(ptr_), father(father_) {}
-    /**
-     * return a new iterator which pointer n-next elements
-     * as well as operator-
-     */
+
     iterator operator+(const int &n) const {
       return iterator(ptr + n, father);
       // TODO
@@ -115,37 +105,27 @@ public:
       return *this;
       // TODO
     }
-    /**
-     * TODO iter++
-     */
+
     iterator operator++(int) {
       ++ptr;
       return iterator(ptr - 1, father);
     }
-    /**
-     * TODO ++iter
-     */
+
     iterator &operator++() {
       ++ptr;
       return *this;
     }
-    /**
-     * TODO iter--
-     */
+
     iterator operator--(int) {
       --ptr;
       return iterator(ptr + 1, father);
     }
-    /**
-     * TODO --iter
-     */
+
     iterator &operator--() {
       --ptr;
       return *this;
     }
-    /**
-     * TODO *it
-     */
+
     T &operator*() const {
 
       if (ptr >= father->size() || ptr < 0) {
@@ -154,10 +134,7 @@ public:
 
       return father->at(ptr);
     }
-    /**
-     * a operator to check whether two iterators are same (pointing to the same
-     * memory address).
-     */
+
     bool operator==(const iterator &rhs) const {
       return father == rhs.father && ptr == rhs.ptr;
     }
@@ -165,9 +142,7 @@ public:
     bool operator==(const const_iterator &rhs) const {
       return father == rhs.father && ptr == rhs.ptr;
     }
-    /**
-     * some other operator for iterator.
-     */
+
     bool operator!=(const iterator &rhs) const {
       return father != rhs.father || ptr != rhs.ptr;
     }
@@ -178,10 +153,7 @@ public:
 
     // operator const_iterator() { return const_iterator(ptr, father); }
   };
-  /**
-   * TODO
-   * has same function as iterator, just for a const object.
-   */
+
   class const_iterator {
   public:
     using difference_type = std::ptrdiff_t;
@@ -198,10 +170,7 @@ public:
   public:
     const_iterator(const std::size_t &ptr_, const vector<T> *father_)
         : ptr(ptr_), father(father_) {}
-    /**
-     * return a new iterator which pointer n-next elements
-     * as well as operator-
-     */
+
     const_iterator operator+(const int &n) const {
       return const_iterator(ptr + n, father);
       // TODO
@@ -234,35 +203,27 @@ public:
       return *this;
       // TODO
     }
-    /**
-     * TODO iter++
-     */
+
     const_iterator operator++(int) {
       ++ptr;
       return const_iterator(ptr - 1, father);
     }
-    /**
-     * TODO ++iter
-     */
+
     const_iterator &operator++() {
       ++ptr;
       return *this;
     }
-    /**
-     * TODO iter--
-     */
+
     const_iterator operator--(int) {
       --ptr;
       return const_iterator(ptr + 1, father);
     }
-    /**
-     * TODO --iter
-     */
+
     const_iterator &operator--() {
       --ptr;
       return *this;
     }
-    /*TODO*/
+
     const T &operator*() const {
 
       if (ptr >= father->size() || ptr < 0) {
@@ -281,9 +242,7 @@ public:
     bool operator==(const const_iterator &rhs) const {
       return father == rhs.father && ptr == rhs.ptr;
     }
-    /**
-     * some other operator for iterator.
-     */
+
     bool operator!=(const iterator &rhs) const {
       return father != rhs.father || ptr != rhs.ptr;
     }
@@ -292,10 +251,7 @@ public:
       return father != rhs.father || ptr != rhs.ptr;
     }
   };
-  /**
-   * TODO Constructs
-   * At least two: default constructor, copy constructor
-   */
+
   vector() : size_(0), block_count(1), head(new T *[block_size]) {}
 
   vector(const vector &other)
@@ -313,9 +269,7 @@ public:
     other.size_ = 0;
     other.block_count = 0;
   }
-  /**
-   * TODO Destructor
-   */
+
   ~vector() {
 
     if (head == nullptr) {
@@ -328,9 +282,7 @@ public:
 
     delete[] head;
   }
-  /**
-   * TODO Assignment operator
-   */
+
   vector<T> &operator=(const vector<T> &other) {
     if (&other == this) {
       return *this;
@@ -375,10 +327,7 @@ public:
 
     return *this;
   }
-  /**
-   * assigns specified element with bounds checking
-   * throw index_out_of_bound if pos is not in [0, size)
-   */
+
   T &at(const size_t &pos) {
 
     if (pos >= size_ || pos < 0) {
@@ -395,19 +344,11 @@ public:
 
     return *(head[pos]);
   }
-  /**
-   * assigns specified element with bounds checking
-   * throw index_out_of_bound if pos is not in [0, size)
-   * !!! Pay attentions
-   *   In STL this operator does not check the boundary but I want you to do.
-   */
+
   T &operator[](const size_t &pos) { return at(pos); }
 
   const T &operator[](const size_t &pos) const { return at(pos); }
-  /**
-   * access the first element.
-   * throw container_is_empty if size == 0
-   */
+
   const T &front() const {
 
     if (size_ == 0) {
@@ -416,10 +357,7 @@ public:
 
     return at(0);
   }
-  /**
-   * access the last element.
-   * throw container_is_empty if size == 0
-   */
+
   const T &back() const {
 
     if (size_ == 0) {
@@ -428,29 +366,19 @@ public:
 
     return at(size_ - 1);
   }
-  /**
-   * returns an iterator to the beginning.
-   */
+
   iterator begin() { return iterator(0, this); }
 
   const_iterator cbegin() const { return const_iterator(0, this); }
-  /**
-   * returns an iterator to the end.
-   */
+
   iterator end() { return iterator(size_, this); }
 
   const_iterator cend() const { return const_iterator(size_, this); }
-  /**
-   * checks whether the container is empty
-   */
+
   bool empty() const { return size_ == 0; }
-  /**
-   * returns the number of elements
-   */
+
   size_t size() const { return size_; }
-  /**
-   * clears the contents
-   */
+
   void clear() {
 
     if (head != nullptr) {
@@ -464,10 +392,7 @@ public:
     block_count = 1;
     size_ = 0;
   }
-  /**
-   * inserts value before pos
-   * returns an iterator pointing to the inserted value.
-   */
+
   iterator insert(iterator pos, const T &value) {
 
     if (pos - begin() > size_) {
@@ -486,13 +411,7 @@ public:
 
     return pos;
   }
-  /**
-   * inserts value at index ind.
-   * after inserting, this->at(ind) == value
-   * returns an iterator pointing to the inserted value.
-   * throw index_out_of_bound if ind > size (in this situation ind can be size
-   * because after inserting the size will increase 1.)
-   */
+
   iterator insert(const size_t &ind, const T &value) {
 
     if (ind > size_) {
@@ -511,12 +430,7 @@ public:
 
     return iterator(ind, this);
   }
-  /**
-   * removes the element at pos.
-   * return an iterator pointing to the following element.
-   * If the iterator pos refers the last element, the end() iterator is
-   * returned.
-   */
+
   iterator erase(iterator pos) {
 
     if (pos - begin() >= size_) {
@@ -537,11 +451,7 @@ public:
 
     return pos;
   }
-  /**
-   * removes the element with index ind.
-   * return an iterator pointing to the following element.
-   * throw index_out_of_bound if ind >= size
-   */
+
   iterator erase(const size_t &ind) {
 
     if (ind >= size_) {
@@ -562,9 +472,7 @@ public:
 
     return iterator(ind, this);
   }
-  /**
-   * adds an element to the end.
-   */
+
   void push_back(const T &value) {
 
     if (size_ + 1 == block_count * block_size) {
@@ -574,10 +482,7 @@ public:
     head[size_] = new T(value);
     ++size_;
   }
-  /**
-   * remove the last element from the end.
-   * throw container_is_empty if size() == 0
-   */
+
   void pop_back() {
 
     if (size_ == 0) {
@@ -630,5 +535,6 @@ private:
     delete[] tmp;
   }
 };
+*/
 
 #endif
